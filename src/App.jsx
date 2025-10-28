3961d77f7421d5360936f6bba1eb6f73d75cdcc6
LauraRo166 <laura.rsanchez@mail.escuelaing.edu.co>
2025-10-28 10:33:05 -0500

    chore: Eliminar carpeta pages, se mueven los archivos a feature/home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./features/home/Home.jsx";
 
 import './styles/App.css'
 
